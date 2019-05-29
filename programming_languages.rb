b385c7a913671d80c941d290f65fc78b85f00ab2
Devin Arnold <dbarnold321@gmail.com>
2019-05-29 19:27:17 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,6 +3,7 @@ require "pry"
 def reformat_languages(languages)
   revised_language = Hash.new
 languages.each do |key, value|
+  revised_language[value] = value[:type]
   binding.pry
   # your code here
 end
