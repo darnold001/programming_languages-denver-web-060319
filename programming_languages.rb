28f29897fd003acdeee68fde2e047c733ec18872
Devin Arnold <dbarnold321@gmail.com>
2019-05-29 19:37:22 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ def reformat_languages(languages)
   revised_language = Hash.new
 languages.each do |key, value|
   value.each do |lan , type|
-    if revised_language[lan][style] << style
+    if revised_language[lan][:style] << style
 else
   revised_language[lan] = type
   revised_language[lan][:style] = [style]
